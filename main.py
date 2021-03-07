a69607fd821ed41eeed4e3b9fd92fdae48a2e3a2
Juan Antonio Pérez <japerez@gmail.com>
2021-03-07 18:19:39 +0100

    Copia de código anterior

--- main.py
+++ main.py
@@ -1,19 +1,247 @@
-my_sprite = sprites.create(img("""
-    . . . . . . f f f f . . . . . .
-    . . . . f f f 2 2 f f f . . . .
-    . . . f f f 2 2 2 2 f f f . . .
-    . . f f f e e e e e e f f f . .
-    . . f f e 2 2 2 2 2 2 e e f . .
-    . . f e 2 f f f f f f 2 e f . .
-    . . f f f f e e e e f f f f . .
-    . f f e f b f 4 4 f b f e f f .
-    . f e e 4 1 f d d f 1 4 e e f .
-    . . f e e d d d d d d e e f . .
-    . . . f e e 4 4 4 4 e e f . . .
-    . . e 4 f 2 2 2 2 2 2 f 4 e . .
-    . . 4 d f 2 2 2 2 2 2 f d 4 . .
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
+
+
+def init_lab (lab:List[List[number]], visitado:List[List[bool]]):
+    for i in range(LADO):
+        lab.append([])
+        visitado.append([])
+        for j in range(LADO):
+            if i%2==0 or j%2==0:
+                lab[i].append(MURO)
+                visitado[i].append(True)
+            else:
+                lab[i].append(PASILLO)
+                visitado[i].append(False)
+
+def pinta_mosaicos(lab:List[List[number]]):
+    for i in range(LADO):
+        for j in range(LADO):
+            if lab[i][j]==MURO:
+                tiles.set_tile_at(tiles.get_tile_location(i, j), sprites.builtin.brick)
+                tiles.set_wall_at(tiles.get_tile_location(i, j), True)
+            elif lab[i][j]==PASILLO:
+                tiles.set_tile_at(tiles.get_tile_location(i, j), sprites.dungeon.dark_ground_center)
+            elif lab[i][j]==PUERTA:
+                tiles.set_tile_at(tiles.get_tile_location(i, j), assets.tile("""
+                puerta
+                """))
+
+
+def vecinos(c:List[number]):
+    v=[]
+    x=c[0]
+    y=c[1]
+    if x-2>0:
+        v.append([x-2,y])
+    if y+2<LADO:
+        v.append([x,y+2])
+    if x+2<LADO:
+        v.append([x+2,y])
+    if y-2>0:
+        v.append([x,y-2])
+    return v
+    
+def vecinos_no_visitados(c:List[number]):
+    todos: List[List[number]]= []
+    todos=vecinos(c)
+    no_visitados: List[List[number]]= []
+    no_visitados=[]
+    for h in todos:
+        if not visitado[h[0]][h[1]]:
+            no_visitados.append(h)
+    return no_visitados
+        
+
+def celda_enmedio(c1:List[number],c2:List[number]):
+    x1= c1[0]
+    y1= c1[1]
+    x2= c2[0]
+    y2= c2[1]
+    if y1==y2:
+        y=y1
+        if x2<x1:
+            x=x1-1
+        else:
+            x=x1+1
+    else:
+        x=x1
+        if y2<y1:
+            y=y1-1
+        else:
+            y=y1+1
+    return [x,y]
+
+def aleatorio_impar(abajo,arriba):
+    nai=randint(abajo,arriba)
+    if nai%2==1:
+        return nai
+    elif nai==arriba:
+        return nai-1
+    else:
+        return nai+1
+
+def crea_laberinto(lab:List[List[number]], visitado:List[List[bool]]):
+    pila: List[List[number]]= []
+    pila.append([aleatorio_impar(0,LADO-1),aleatorio_impar(0,LADO-1)])
+    while len(pila)!=0:
+        actual=pila[len(pila)-1]
+        visitado[actual[0]][actual[1]]= True
+        vecinitos=vecinos_no_visitados(actual)
+        if len(vecinitos)==0:
+            pila.pop()
+        else:
+            n=randint(0, len(vecinitos)-1)
+            sig=vecinitos[n]
+            muro=celda_enmedio(actual,sig)
+            lab[muro[0]][muro[1]]= PASILLO
+            pila.append(sig)
+
+def crea_puerta(lab:List[List[number]]):
+    if borde==0:  # arriba
+        y=0
+        x=aleatorio_impar(0, LADO-1)
+    elif borde==1:  # abajo
+        y=LADO-1
+        x=aleatorio_impar(0, LADO-1)
+    elif borde==2: # izquierda
+        y=aleatorio_impar(0, LADO-1)
+        x=0
+    else: # derecha
+        y=aleatorio_impar(0, LADO-1)
+        x=LADO-1
+    lab[x][y]=PUERTA
+
+def crea_cruz(lab:List[List[number]], visitado:List[List[bool]]):
+    celda=[3,3]
+    vecinitos: List[List[number]]= []
+    vecinitos=vecinos(celda)
+    for v in vecinitos:
+        muro=celda_enmedio(celda,v)
+        xmuro= muro[0]
+        ymuro= muro[1]
+        lab[xmuro][ymuro]=PASILLO
+
+def choca_chup(p,f):
+    if armadillo:
+        f.destroy(effects.fire, 500)
+    else:
+        game.over()
+
+def coge_arma(p,a):
+    global armadillo
+    armadillo=True
+    a.destroy(effects.star_field,500)
+
+
+@namespace
+class SpriteKind:
+    fantasma=SpriteKind.create()
+    arma=SpriteKind.create()
+
+# LADO impar máximo de 255
+LADO=7
+MURO=0
+PASILLO=1
+PUERTA=2
+NUM_FANTASMAS=1
+NUM_ARMAS=NUM_FANTASMAS*2
+borde=randint(0,3)
+armadillo=False
+
+#if teseo.overlaps_with():
+    #    game.over(True)
+
+
+
+lab: List[List[number]] = []
+visitado: List[List[bool]] = []
+
+
+sprites.on_overlap(SpriteKind.player, SpriteKind.fantasma, choca_chup)
+sprites.on_overlap(SpriteKind.player, SpriteKind.arma, coge_arma)
+
+scene.set_background_color(3)
+teseo = sprites.create(img("""
+    . . . . . . . . . . . . . . . .
     . . . . . f f f f f f . . . . .
-    . . . . . f f . . f f . . . . .
-"""))
-controller.move_sprite(my_sprite)
+    . . . f f e e e e f 2 f . . . .
+    . . f f e e e e f 2 2 2 f . . .
+    . . f e e e f f 2 2 2 2 f . . .
+    . . f f f f 2 2 2 2 2 2 2 f . .
+    . . f 2 2 2 2 f f f f 2 2 f . .
+    . f f f f f f f e e e f f f . .
+    . f f e 4 4 4 b f 4 4 e e f . .
+    . f e e 4 d 4 1 f d d e f . . .
+    . . f e e e e e d d d f . . . .
+    . . . f f 4 d d e 4 5 f . . . .
+    . . . . f e d d e 2 2 f f . . .
+    . . . . f f e e f 5 4 f f . . .
+    . . . . f f f f f f f f . . . .
+    . . . . f f . . . f f f . . . .
+"""), SpriteKind.player)
+for i in range (NUM_FANTASMAS):
+    fantasma = sprites.create(img("""
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f11111111f.......
+        ......fd11111111df......
+        ......fd11111111df......
+        ......fddd1111dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf11fcdcf......
+        .......fb111111bf.......
+        .....ffffdb11bdffff.....
+        ....fc111cfbbfc111cf....
+        ....f1b1b1ffff1b1b1f....
+        ....fbfbfbffffbfbfbf....
+        .........ffffff.........
+        ..........fff...........
+        ...........f............
+        ........................
+        ........................
+        ........................
+    """), SpriteKind.fantasma)
+    fantasma.set_position(aleatorio_impar(0, LADO-1)*16+8,aleatorio_impar(0, LADO-1)*16+8 )    
+    fantasma.follow(teseo,40)
+for i in range (NUM_ARMAS):
+    fantasma = sprites.create(img("""
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . c c . . . . . . . .
+        . . . . c a f b c . . . . . . .
+        . . . . b f f b c c . . . . . .
+        . . . a a f b a b a c . . . . .
+        . . . c a c b b f f b . . . . .
+        . . . . b f f b f a b . . . . .
+        . . . . a f f b b b a . . . . .
+        . . . . . a b b c c . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    """), SpriteKind.arma)
+    fantasma.set_position(aleatorio_impar(0, LADO-1)*16+8,aleatorio_impar(0, LADO-1)*16+8 )    
+
+teseo.set_position(aleatorio_impar(0, LADO-1)*16+8,aleatorio_impar(0, LADO-1)*16+8)
+controller.move_sprite(teseo)
+teseo.set_bounce_on_wall(True)
+# mapa del tamaño máximo: 255x255
+tiles.set_tilemap(tilemap("""level1"""))
+
+
+
+
+
+
+scene.camera_follow_sprite(teseo)
+
+init_lab(lab, visitado)
+crea_laberinto(lab, visitado)
+crea_puerta(lab)
+pinta_mosaicos(lab)
--- main.ts
+++ main.ts
@@ -1,19 +1,289 @@
-let my_sprite = sprites.create(img`
-    . . . . . . f f f f . . . . . .
-    . . . . f f f 2 2 f f f . . . .
-    . . . f f f 2 2 2 2 f f f . . .
-    . . f f f e e e e e e f f f . .
-    . . f f e 2 2 2 2 2 2 e e f . .
-    . . f e 2 f f f f f f 2 e f . .
-    . . f f f f e e e e f f f f . .
-    . f f e f b f 4 4 f b f e f f .
-    . f e e 4 1 f d d f 1 4 e e f .
-    . . f e e d d d d d d e e f . .
-    . . . f e e 4 4 4 4 e e f . . .
-    . . e 4 f 2 2 2 2 2 2 f 4 e . .
-    . . 4 d f 2 2 2 2 2 2 f d 4 . .
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
+let i: number;
+let fantasma: Sprite;
+function init_lab(lab: number[][], visitado: boolean[][]) {
+    for (let i = 0; i < LADO; i++) {
+        lab.push([])
+        visitado.push([])
+        for (let j = 0; j < LADO; j++) {
+            if (i % 2 == 0 || j % 2 == 0) {
+                lab[i].push(MURO)
+                visitado[i].push(true)
+            } else {
+                lab[i].push(PASILLO)
+                visitado[i].push(false)
+            }
+            
+        }
+    }
+}
+
+function pinta_mosaicos(lab: number[][]) {
+    for (let i = 0; i < LADO; i++) {
+        for (let j = 0; j < LADO; j++) {
+            if (lab[i][j] == MURO) {
+                tiles.setTileAt(tiles.getTileLocation(i, j), sprites.builtin.brick)
+                tiles.setWallAt(tiles.getTileLocation(i, j), true)
+            } else if (lab[i][j] == PASILLO) {
+                tiles.setTileAt(tiles.getTileLocation(i, j), sprites.dungeon.darkGroundCenter)
+            } else if (lab[i][j] == PUERTA) {
+                tiles.setTileAt(tiles.getTileLocation(i, j), assets.tile`
+                puerta
+                `)
+            }
+            
+        }
+    }
+}
+
+function vecinos(c: number[]): number[][] {
+    let v = []
+    let x = c[0]
+    let y = c[1]
+    if (x - 2 > 0) {
+        v.push([x - 2, y])
+    }
+    
+    if (y + 2 < LADO) {
+        v.push([x, y + 2])
+    }
+    
+    if (x + 2 < LADO) {
+        v.push([x + 2, y])
+    }
+    
+    if (y - 2 > 0) {
+        v.push([x, y - 2])
+    }
+    
+    return v
+}
+
+function vecinos_no_visitados(c: number[]): number[][] {
+    let todos : number[][] = []
+    todos = vecinos(c)
+    let no_visitados : number[][] = []
+    no_visitados = []
+    for (let h of todos) {
+        if (!visitado[h[0]][h[1]]) {
+            no_visitados.push(h)
+        }
+        
+    }
+    return no_visitados
+}
+
+function celda_enmedio(c1: number[], c2: number[]): number[] {
+    let y: number;
+    let x: number;
+    let x1 = c1[0]
+    let y1 = c1[1]
+    let x2 = c2[0]
+    let y2 = c2[1]
+    if (y1 == y2) {
+        y = y1
+        if (x2 < x1) {
+            x = x1 - 1
+        } else {
+            x = x1 + 1
+        }
+        
+    } else {
+        x = x1
+        if (y2 < y1) {
+            y = y1 - 1
+        } else {
+            y = y1 + 1
+        }
+        
+    }
+    
+    return [x, y]
+}
+
+function aleatorio_impar(abajo: number, arriba: number): number {
+    let nai = randint(abajo, arriba)
+    if (nai % 2 == 1) {
+        return nai
+    } else if (nai == arriba) {
+        return nai - 1
+    } else {
+        return nai + 1
+    }
+    
+}
+
+function crea_laberinto(lab: number[][], visitado: boolean[][]) {
+    let actual: number[];
+    let vecinitos: number[][];
+    let n: number;
+    let sig: number[];
+    let muro: number[];
+    let pila : number[][] = []
+    pila.push([aleatorio_impar(0, LADO - 1), aleatorio_impar(0, LADO - 1)])
+    while (pila.length != 0) {
+        actual = pila[pila.length - 1]
+        visitado[actual[0]][actual[1]] = true
+        vecinitos = vecinos_no_visitados(actual)
+        if (vecinitos.length == 0) {
+            pila.pop()
+        } else {
+            n = randint(0, vecinitos.length - 1)
+            sig = vecinitos[n]
+            muro = celda_enmedio(actual, sig)
+            lab[muro[0]][muro[1]] = PASILLO
+            pila.push(sig)
+        }
+        
+    }
+}
+
+function crea_puerta(lab: number[][]) {
+    let y: number;
+    let x: number;
+    if (borde == 0) {
+        //  arriba
+        y = 0
+        x = aleatorio_impar(0, LADO - 1)
+    } else if (borde == 1) {
+        //  abajo
+        y = LADO - 1
+        x = aleatorio_impar(0, LADO - 1)
+    } else if (borde == 2) {
+        //  izquierda
+        y = aleatorio_impar(0, LADO - 1)
+        x = 0
+    } else {
+        //  derecha
+        y = aleatorio_impar(0, LADO - 1)
+        x = LADO - 1
+    }
+    
+    lab[x][y] = PUERTA
+}
+
+function crea_cruz(lab: number[][], visitado: boolean[][]) {
+    let muro: number[];
+    let xmuro: number;
+    let ymuro: number;
+    let celda = [3, 3]
+    let vecinitos : number[][] = []
+    vecinitos = vecinos(celda)
+    for (let v of vecinitos) {
+        muro = celda_enmedio(celda, v)
+        xmuro = muro[0]
+        ymuro = muro[1]
+        lab[xmuro][ymuro] = PASILLO
+    }
+}
+
+namespace SpriteKind {
+    export const fantasma = SpriteKind.create()
+    export const arma = SpriteKind.create()
+}
+
+//  LADO impar máximo de 255
+let LADO = 7
+let MURO = 0
+let PASILLO = 1
+let PUERTA = 2
+let NUM_FANTASMAS = 1
+let NUM_ARMAS = NUM_FANTASMAS * 2
+let borde = randint(0, 3)
+let armadillo = false
+// if teseo.overlaps_with():
+//     game.over(True)
+let lab : number[][] = []
+let visitado : boolean[][] = []
+sprites.onOverlap(SpriteKind.Player, SpriteKind.fantasma, function choca_chup(p: Sprite, f: Sprite) {
+    if (armadillo) {
+        f.destroy(effects.fire, 500)
+    } else {
+        game.over()
+    }
+    
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.arma, function coge_arma(p: Sprite, a: Sprite) {
+    
+    armadillo = true
+    a.destroy(effects.starField, 500)
+})
+scene.setBackgroundColor(3)
+let teseo = sprites.create(img`
+    . . . . . . . . . . . . . . . .
     . . . . . f f f f f f . . . . .
-    . . . . . f f . . f f . . . . .
-`)
-controller.moveSprite(my_sprite)
+    . . . f f e e e e f 2 f . . . .
+    . . f f e e e e f 2 2 2 f . . .
+    . . f e e e f f 2 2 2 2 f . . .
+    . . f f f f 2 2 2 2 2 2 2 f . .
+    . . f 2 2 2 2 f f f f 2 2 f . .
+    . f f f f f f f e e e f f f . .
+    . f f e 4 4 4 b f 4 4 e e f . .
+    . f e e 4 d 4 1 f d d e f . . .
+    . . f e e e e e d d d f . . . .
+    . . . f f 4 d d e 4 5 f . . . .
+    . . . . f e d d e 2 2 f f . . .
+    . . . . f f e e f 5 4 f f . . .
+    . . . . f f f f f f f f . . . .
+    . . . . f f . . . f f f . . . .
+`, SpriteKind.Player)
+for (i = 0; i < NUM_FANTASMAS; i++) {
+    fantasma = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f11111111f.......
+        ......fd11111111df......
+        ......fd11111111df......
+        ......fddd1111dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf11fcdcf......
+        .......fb111111bf.......
+        .....ffffdb11bdffff.....
+        ....fc111cfbbfc111cf....
+        ....f1b1b1ffff1b1b1f....
+        ....fbfbfbffffbfbfbf....
+        .........ffffff.........
+        ..........fff...........
+        ...........f............
+        ........................
+        ........................
+        ........................
+    `, SpriteKind.fantasma)
+    fantasma.setPosition(aleatorio_impar(0, LADO - 1) * 16 + 8, aleatorio_impar(0, LADO - 1) * 16 + 8)
+    fantasma.follow(teseo, 40)
+}
+for (i = 0; i < NUM_ARMAS; i++) {
+    fantasma = sprites.create(img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . c c . . . . . . . .
+        . . . . c a f b c . . . . . . .
+        . . . . b f f b c c . . . . . .
+        . . . a a f b a b a c . . . . .
+        . . . c a c b b f f b . . . . .
+        . . . . b f f b f a b . . . . .
+        . . . . a f f b b b a . . . . .
+        . . . . . a b b c c . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `, SpriteKind.arma)
+    fantasma.setPosition(aleatorio_impar(0, LADO - 1) * 16 + 8, aleatorio_impar(0, LADO - 1) * 16 + 8)
+}
+teseo.setPosition(aleatorio_impar(0, LADO - 1) * 16 + 8, aleatorio_impar(0, LADO - 1) * 16 + 8)
+controller.moveSprite(teseo)
+teseo.setBounceOnWall(true)
+//  mapa del tamaño máximo: 255x255
+tiles.setTilemap(tilemap`level1`)
+scene.cameraFollowSprite(teseo)
+init_lab(lab, visitado)
+crea_laberinto(lab, visitado)
+crea_puerta(lab)
+pinta_mosaicos(lab)
